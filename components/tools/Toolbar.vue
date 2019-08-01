f7c134b6b06e74b319048cc88fbea1811a40083c
Michael J. Wilson <wilsongis@gmail.com>
2019-08-01 11:32:30 -0500

    Initial Toolbar working

---
+++ components/tools/Toolbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="toolbar">
+    <v-toolbar dense floating>
+      <v-text-field
+        hide-details
+        prepend-icon="search"
+        single-line
+      ></v-text-field>
+
+      <v-btn icon>
+        <v-icon>my_location</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>more_vert</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </div>
+</template>
+<script>
+export default {
+  props: ['toolbar']
+}
+</script>
