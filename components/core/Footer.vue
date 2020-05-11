003494f8badb55fa53a20245ca8b6819808e9c84
Mike Wilson <wilsongis@gmail.com>
2020-05-11 12:33:38 -0500

    Updated footer date

--- components/core/Footer.vue
+++ components/core/Footer.vue
@@ -2,6 +2,6 @@
   <v-footer app color="blue darken-3" class="white--text">
     <span>APSU GIS Center</span>
     <v-spacer></v-spacer>
-    <span>&copy; 2019</span>
+    <span>&copy; 2020</span>
   </v-footer>
 </template>
